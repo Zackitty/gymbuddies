89dc87edea7160fc824bbebc8c04c368d7e2d10f
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-01-01 22:10:52 -0600

    add views

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls import url
+from django.contrib import admin
+from gymapp import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'admin/', admin.site.urls),
+    url(r'^idealweight/',views.IdealWeight)
 ]
