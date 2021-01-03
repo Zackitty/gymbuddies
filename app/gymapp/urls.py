ff0c7595586897ec44d976ff78d07c7fd40dfe26
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-01-02 22:17:55 -0600

    fix urls

--- app/gymapp/urls.py
+++ app/gymapp/urls.py
@@ -1,10 +1,7 @@
 
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
-from django.contrib import admin
-from gymapp import views
+from django.urls import path
+from .views import main
+
 urlpatterns = [
-    path(r'admin/', admin.site.urls),
-    path('', include('gymapp.urls'))
+    path('', main)
 ]
