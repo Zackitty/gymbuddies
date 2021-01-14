8da2d06a62fc2c9b882f67f5df7a1e69a6782580
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-01-14 15:58:22 -0600

    Add Django models

--- app/gymapp/models.py
+++ app/gymapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+
+class Exercise(models.Model):
+
+class Friend(models.Model):
+
+class Loss(models.Model):
+
+class Gain(models.Model): 
+
+class Exercise(models.Model):      
