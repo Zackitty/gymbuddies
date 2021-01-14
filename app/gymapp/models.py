c00ad0b5681d7110fa68e561252df2c5eeaff035
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-01-14 16:17:58 -0600

    Add User Model

--- app/gymapp/models.py
+++ app/gymapp/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    full_name = models.CharField(max_length=20, default="", unique=False)
+    username = models.CharField(max_length=20, default="", unique=True)
+    password = 
+    weight = models.IntegerField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=20, default="", unique=False)
+    goal = models.CharField(max_length=20, default="", unique=False)
 
 class Exercise(models.Model):
 
