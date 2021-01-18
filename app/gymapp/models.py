a8d838e58a6348d8586b82fa703ea24fb6de92b8
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-01-18 17:06:12 -0600

    fix models

--- app/gymapp/models.py
+++ app/gymapp/models.py
@@ -28,25 +28,19 @@ class Friend(models.Model):
 
 
 class Loss(models.Model):
-    loss_id = models.BinaryField(primary_key=True)
     amount = models.IntegerField()
-    loser_id = models.IntegerField()
     entry_date = models.DateTimeField(auto_now_add=True)
     loser = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Gain(models.Model): 
-    gain_id = models.BinaryField(primary_key=True)
     amount = models.IntegerField()
-    gainer_id = models.IntegerField()
     entry_date = models.DateTimeField(auto_now_add=True)
     gainer = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Lift(models.Model): 
-    lift_id = models.BinaryField(primary_key=True) 
     name = models.CharField(max_length=20, default="", unique=True)
     weight = models.IntegerField()
     one_rep_max = models.IntegerField()
     reps = models.IntegerField()
-    lifter_id = models.IntegerField()
     entry_date = models.DateTimeField(auto_now_add=True)
     lifter = models.ForeignKey(User, on_delete=models.CASCADE)
