27ea856334c1925ed0b0b020b1052f3b0ab1173c
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-01-19 12:52:34 -0600

    import serializers

--- app/gymapp/views.py
+++ app/gymapp/views.py
@@ -7,13 +7,14 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from django.http import HttpResponse
 from django.core import serializers
 from django.conf import settings
 import json
+from .serializers import UserSerializer, LiftSerializer, FriendSerializer, LossSerializer, GainSerializer, ExerciseSerializer
 # Create your views here.
 
  
-def main(request):
-    return HttpResponse('Hello')
+class UserView(generics.CreateAPIView):
+    queryset = User.Objects.All()
