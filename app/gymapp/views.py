e9dea160afb86ee2f8d48845c6f8fb62f47ae2a3
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-01-19 12:55:32 -0600

    import models

--- app/gymapp/views.py
+++ app/gymapp/views.py
@@ -12,9 +12,25 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.conf import settings
 import json
+from .models import User, Lift, Friend, Loss, Gain, Exercise
 from .serializers import UserSerializer, LiftSerializer, FriendSerializer, LossSerializer, GainSerializer, ExerciseSerializer
 # Create your views here.
 
  
 class UserView(generics.CreateAPIView):
     queryset = User.Objects.All()
+
+class LiftView(generics.CreateAPIView):
+    queryset = Lift.Objects.All()
+    
+class FriendView(generics.CreateAPIView):
+    queryset = Friend.Objects.All()
+
+class LossView(generics.CreateAPIView):
+    queryset = Loss.Objects.All()
+
+class GainView(generics.CreateAPIView):
+    queryset = Gain.Objects.All()
+
+class ExerciseView(generics.CreateAPIView):
+    queryset = Exercise.Objects.All()
