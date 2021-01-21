16bca6c26a47debbfdeabb0919ad94c2dc076ec1
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-01-21 16:28:34 -0600

    fix urls

--- app/gymapp/urls.py
+++ app/gymapp/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import UserView, LiftView, FriendView, LossView, GainView, ExerciseView
 
 urlpatterns = [
-    path('users', UserView.as_view(), name='homepage'),
-    path('lifts', LiftView),
-    path('friends', FriendView),
+    path('users', UserView.as_view()),
+    path('lifts', LiftView.as_view()),
+    path('friends', FriendView.as_view()),
     path('losses', LossView),
     path('gains', GainView),
     path('exercises', ExerciseView)
