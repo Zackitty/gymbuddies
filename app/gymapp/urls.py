715ecdca718f97d265314257e16edc7f5fd63e5a
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-01-21 16:28:44 -0600

    fix urls

--- app/gymapp/urls.py
+++ app/gymapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('users', UserView.as_view()),
     path('lifts', LiftView.as_view()),
     path('friends', FriendView.as_view()),
-    path('losses', LossView),
+    path('losses', LossView.as_view()),
     path('gains', GainView),
     path('exercises', ExerciseView)
 
