8d3e918de2f678daf8cdad7220ff1ba02b373c82
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-02-26 16:30:16 -0600

    add psql database

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,9 +76,20 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'gymbuddies',
+
+        'USER': 'gymowner',
+
+        'PASSWORD': 'gym',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
