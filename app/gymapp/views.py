e0c72e7bdc3d49fd30301e0eee4752b41e1343e2
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-03-02 14:53:12 -0600

    create user backend

--- app/gymapp/views.py
+++ app/gymapp/views.py
@@ -12,6 +12,7 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.conf import settings
 import json
+import bcrypt
 from .models import User, Lift, Friend, Loss, Gain, Exercise, LiftSet, Exerciser
 from .serializers import UserSerializer, LiftSetSerializer, LiftSerializer, FriendSerializer, LossSerializer, GainSerializer, ExerciseSerializer, ExerciserSerializer
 # Create your views here.
@@ -21,6 +22,24 @@ class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def createUser(self, request):
+        if request.method == 'POST':
+            full_name = request.POST['full_name']
+            username = request.POST['username']
+            password = request.POST['password']
+            weight = request.POST['password']
+            age = request.POST['age']
+            gender = request.POST['gender']
+            goal = request.POST['goal']
+
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(14))
+            
+            created_user = User(full_name=full_name, username=username, password=hashed_password,
+                                weight=weight, age=age, gender=gender, goal=goal)
+
+            created_user.save()
+
+
 class LiftView(generics.CreateAPIView):
     queryset = Lift.objects.all()
     serializer_class = LiftSerializer
@@ -28,6 +47,7 @@ class LiftView(generics.CreateAPIView):
 class LiftSetView(generics.CreateAPIView):
     queryset = LiftSet.objects.all()
     serializer_class = LiftSetSerializer
+    
 
 class FriendView(generics.CreateAPIView):
     queryset = Friend.objects.all()
