c98e391198ce2895aa025ef991ad6d999c07a3d7
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-03-06 16:32:14 -0600

    add logic forediting

--- app/gymapp/urls.py
+++ app/gymapp/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import UserView, LiftView, LiftSetView, FriendView, LossView, GainView, ExerciseView, ExerciserView, getLift
+from .views import UserView, LiftView, LiftSetView, FriendView, LossView, GainView, ExerciseView, ExerciserView, getLift, getUser
 
 urlpatterns = [
     path('users', UserView.as_view()),
+    path('users/<path>', getUser),
     path('lifts', LiftView.as_view()),
     path('lifts/<path>', getLift),
     path('liftsets', LiftView.as_view()),
--- app/gymapp/views.py
+++ app/gymapp/views.py
@@ -13,6 +13,7 @@ from django.core import serializers
 from django.conf import settings
 import json
 import bcrypt
+from django.views.decorators.csrf import csrf_exempt
 from .models import User, Lift, Friend, Loss, Gain, Exercise, LiftSet, Exerciser
 from .serializers import UserSerializer, LiftSetSerializer, LiftSerializer, FriendSerializer, LossSerializer, GainSerializer, ExerciseSerializer, ExerciserSerializer
 # Create your views here.
@@ -21,20 +22,39 @@ class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class LiftView(generics.CreateAPIView):
+def getUser(request, path):
+    if request.method == 'GET':
+        qs = Lift.objects.get(name=path)
+        serialized_obj = serializers.serialize('json', [ qs, ])
+        jsOb = serialized_obj[1]
+        print(qs.name)
+        return HttpResponse(serialized_obj, content_type="application/x-javascript")
+    if request.method == 'POST':
+        qs = Lift.objects.get(name=path)
+        name = request.POST.get("name")
+        qs.name = name
+        qs.save()
+        return HttpResponse(qs)
+
+class LiftView(generics.ListCreateAPIView):
     queryset = Lift.objects.all()
     serializer_class = LiftSerializer
-  
 
-
+@csrf_exempt
 def getLift(request, path):
-    qs = Lift.objects.get(name=path)
-    print(qs)
-    serialized_obj = serializers.serialize('json', [ qs, ])
-    return HttpResponse(serialized_obj, content_type="application/x-javascript")
-    
-    
-    
+    if request.method == 'GET':
+        qs = Lift.objects.get(name=path)
+        serialized_obj = serializers.serialize('json', [ qs, ])
+        jsOb = serialized_obj[1]
+        print(qs.name)
+        return HttpResponse(serialized_obj, content_type="application/x-javascript")
+    if request.method == 'POST':
+        qs = Lift.objects.get(name=path)
+        name = request.POST.get("name")
+        qs.name = name
+        qs.save()
+        return HttpResponse(qs)
+
 class LiftSetView(generics.CreateAPIView):
     queryset = LiftSet.objects.all()
     serializer_class = LiftSetSerializer
