95f09984edb89bd94702f5011922193754cd0fce
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-03-13 20:24:58 -0600

    configurestore

---
+++ client/store/configurestore.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist'
+import rootReducer from './rootreducer'
+import storage from 'redux-persist/lib/storage' 
+import AsyncStorage from '@react-native-community/async-storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+ };
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) 
+
+
+ export const store = createStore(
+  persistedReducer,
+  {},
+    composeEnhancers(applyMiddleware(thunk)),
+  );
+
+export const persistor = persistStore(store)
