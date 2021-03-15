5d0a5b95795a6caf53731a74cdac14fec1c372cc
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-03-14 20:23:48 -0500

    add auth to store

---
+++ client/store/rootreducer.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import currentUser from './auth'
+import users from './users'
+import chat from './chat'
+
+
+
+
+
+
+const rootReducer = combineReducers({
+  currentUser,
+  users, 
+  chat
+  //ADD ADDITIONAL SLICES OF STATE HERE 
+});
+
+export default rootReducer
