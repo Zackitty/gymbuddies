02c414948f667b032595c8a5ac164f896cafe752
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-03-18 15:24:40 -0500

    break down into smaller folders

--- client/App.js
+++ client/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import HomeScreen from './components/auth/HomeScreen'
-import ProfileScreen from './components/auth/ProfileScreen'
+import HomeScreen from './components/auth/home/HomeScreen'
+import ProfileScreen from './components/auth/profile/ProfileScreen'
 import { NavigationContainer } from '@react-navigation/native' 
 import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
