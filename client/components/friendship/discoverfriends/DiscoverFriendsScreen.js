d94ed616cf8c6b03b3b709d28b7c353f467fa3fd
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-03-30 20:51:27 -0500

    friendsw

--- client/components/friendship/discoverfriends/DiscoverFriendsScreen.js
+++ client/components/friendship/discoverfriends/DiscoverFriendsScreen.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { useSelector } from 'react-redux';
 import { CommonActions } from '@react-navigation/native';
-
+import FriendBox from '../friend/FriendBox'
 const DiscoverFriendsScreen = ({ navigation, route }) => {
+    const [friendsScroll, setFriendsScroll] = useState([])
 
 return (
 
   <View>
-  
+  {friendsScroll.map((friend, i) => <FriendBox us/>)}
   </View>
 
 )
--- client/components/friendship/friend/FriendBox.js
+++ client/components/friendship/friend/FriendBox.js
@@ -3,12 +3,19 @@ import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { useSelector } from 'react-redux';
 import { CommonActions } from '@react-navigation/native';
 
-const FriendBox = ({ navigation, route }) => {
-
+const FriendBox = ({ username, weight, age, gender, goal }) => {
+const handleAdd = async(e) => {
+  
+}
 return (
 
   <View>
-  
+    <Text>{username}</Text>
+    <Text>{age}</Text>
+    <Text>{weight}</Text>
+    <Text>{gender}</Text>
+    <Text>{goal}</Text>
+    <Button title="Add Friend" onPress={handleAdd}></Button>
   </View>
 
 )
