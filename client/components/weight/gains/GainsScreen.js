cae22f91e4d10c352be99bcba39d88d1bf983df8
Zackitty <59539209+Zackitty@users.noreply.github.com>
2021-04-13 14:33:32 -0500

    create new components for lift and exercise

--- client/components/weight/gains/GainsScreen.js
+++ client/components/weight/gains/GainsScreen.js
@@ -4,12 +4,23 @@ import { useSelector } from 'react-redux';
 import { CommonActions } from '@react-navigation/native';
 
 const GainsScreen = ({ navigation, route }) => {
+  const [discoverFriendsScroll, setDiscoverFriendsScroll] = useState([])
+  
+  useEffect(() => {
+    gainsFeed()
+  }, [])
 
+const gainsFeed = async (e) => {
+  await fetch(`${apiUrl}/users/totalloss/${data[key].total_lozz_id}`)
+  .then(res => res.json())
+  .then(data =>  keyObj['totalloss'] = data[0].fields)   
+}
 return (
 
 <View>
-  
-  </View>
+
+
+</View>
 
 )
 
--- client/components/weight/loss/LossesScreen.js
+++ client/components/weight/loss/LossesScreen.js
@@ -5,6 +5,17 @@ import { CommonActions } from '@react-navigation/native';
 
 const LossesScreen = ({ navigation, route }) => {
 
+  const [discoverFriendsScroll, setDiscoverFriendsScroll] = useState([])
+  
+  useEffect(() => {
+    gainsFeed()
+  }, [])
+
+const gainsFeed = async (e) => {
+  await fetch(`${apiUrl}/users/totalloss/${data[key].total_lozz_id}`)
+  .then(res => res.json())
+  .then(data =>  keyObj['totalloss'] = data[0].fields)   
+}
 return (<View>
   
 </View>)
